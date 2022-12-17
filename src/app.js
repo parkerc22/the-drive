d8ee9d044b17d5a080dc884e1022fde7985f1fa7
Parker Connelly <parkerc@princeton.edu>
2022-12-16 22:54:15 -0500

    shading!

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import { MakeAudio } from './components/audio';
 import { InputControl, PlayerVehicle } from './components/input';
 
 
-import { GamePhysicsScene,  FrustumCulling, GameScene, SceneCustom, TestScene } from './components/scenes';
+import { GamePhysicsScene, FrustumCulling, GameScene, SceneCustom, TestScene } from './components/scenes';
 import { World, Vec3, Body, Sphere, Plane, Box, Material, Cylinder, Ray, Trimesh, Quaternion, ConvexPolyhedron, RigidVehicle } from 'cannon-es'
 
 import CannonDebugger from 'cannon-es-debugger';
@@ -33,7 +33,7 @@ import { Menu } from './components/objects/Menu';
 import { MaterialLoader } from 'three';
 
 
-import {GameOver} from './components/objects/GameOver';
+import { GameOver } from './components/objects/GameOver';
 
 
 // // load in shaders
@@ -56,8 +56,8 @@ const renderer = new WebGLRenderer({ antialias: true });
 const hud = new HUD();
 const hudCamera = new PerspectiveCamera();
 
-hudCamera.position.set(0,20,0);
-hudCamera.lookAt(new Vector3(0,0,0));
+hudCamera.position.set(0, 20, 0);
+hudCamera.lookAt(new Vector3(0, 0, 0));
 
 let miniMap = false;
 let inMenu = true;
@@ -76,14 +76,14 @@ overheadCamera.position.set(0, 10, 100);
 overheadCamera.lookAt(new Vector3(0, 0, 0));
 overheadCamera.zoom = 0.2;
 overheadCamera.updateProjectionMatrix();
-overheadCamera.up.set(0,-1,0);
+overheadCamera.up.set(0, -1, 0);
 
 /* CITATION: https://stackoverflow.com/questions/63872740/three-js-scaling-a-plane-to-full-screen 
 */
-const menuCamera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+const menuCamera = new OrthographicCamera(- 1, 1, 1, - 1, 0, 1);
 //menuCamera.position.set(0, 300, -100);
 //camera.lookAt(new Vector3(0, 0, 0));
- 
+
 
 // the menu is just a scene
 //const menu = new Menu(window.innerWidth, window.innerHeight, camera.quaternion);
@@ -123,7 +123,7 @@ scene.add(particleSystem);
 // gives each particle a slight initial velocity in x/z directions
 const particleVelocities = [];
 for (let i = 0; i < particleCount; i++) {
-    particleVelocities[i] = [(Math.random() - .5) / 4, (Math.random() - .5) / 4];  
+    particleVelocities[i] = [(Math.random() - .5) / 4, (Math.random() - .5) / 4];
 }
 
 
@@ -187,7 +187,7 @@ const world = new World(
 )
 
 
-const player = new PlayerVehicle(world, [3.1,16, 100]);
+const player = new PlayerVehicle(world, [3.1, 16, 100]);
 
 /*
 const boxBody = new Body({
@@ -202,7 +202,7 @@ const boxBody = new Body({
 
 })*/
 const boxBody = player.chassis;
-boxBody.quaternion.setFromEuler(-Math.PI/2, Math.PI/2, 0);
+boxBody.quaternion.setFromEuler(-Math.PI / 2, Math.PI / 2, 0);
 console.log(boxBody.position)
 
 
@@ -210,7 +210,7 @@ const materialBox = new MeshNormalMaterial()
 const box_geo = new BoxGeometry(1, 1, 2);
 const boxMesh = new Mesh(box_geo, materialBox);
 
-const materialWheel = new MeshBasicMaterial({color: 0xffc0cb});
+const materialWheel = new MeshBasicMaterial({ color: 0xffc0cb });
 materialWheel.transparent = true;
 materialWheel.opacity = 0.5;
 
@@ -218,19 +218,16 @@ const wheelBodies = player.vehicle.wheelBodies;
 console.log(wheelBodies.length);
 const sphere_geo = new SphereGeometry(wheelBodies[0].shapes[0].radius);
 
-const threeArray = new Array(); 
-const cannonArray = new Array(); 
+const threeArray = new Array();
+const cannonArray = new Array();
 
-const anotherLight = new PointLight(0xFFFFFF, 1.2)
-anotherLight.position.add(new Vector3(3.1,16, 100))
-scene.add(anotherLight)
 
 threeArray.push(boxMesh);
 cannonArray.push(boxBody);
 boxBody.sleepSpeedLimit = 1.0
 scene.add(boxMesh)
 
-for(let i = 0; i < wheelBodies.length; i++ ){
+for (let i = 0; i < wheelBodies.length; i++) {
     const circle = new Mesh(sphere_geo, materialWheel);
     threeArray.push(circle);
     cannonArray.push(wheelBodies[i]);
@@ -268,24 +265,114 @@ const roadMat = new MeshToonMaterial({
     side: DoubleSide
 })
 
+
+const _VS = `
+varying vec3 v_normal;
+varying vec3 v_position;
+void main() {
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  v_normal = normal;
+  v_position = position;
+}
+`;
+const _FS = `
+
+varying vec3 v_normal;
+varying vec3 v_position;
+
+struct Data {
+    vec3 position;
+    vec3 direction;
+    vec3 color;
+};
+
+uniform Data data;
+
+void main(){
+    vec3 lightVec = normalize(data.position - v_position);
+    float value = max(0.0, dot(v_normal, lightVec));
+    vec3 color = data.color;
+    if (value > 0.8){
+        color *= 0.8;
+    } else if (value > 0.7) {
+        color *= 0.7; 
+    } else if (value > 0.6) {
+        color *= 0.6; 
+    } else if (value > 0.4) {
+        color*= 0.4;
+    } else if (value > 0.2){
+        color *= 0.2;
+    } else {
+        color *= 0.0;
+    }
+  gl_FragColor = vec4(color, 1.0);
+}
+`;
+
+const anotherLight = new PointLight(0x706f6f, 1.2)
+anotherLight.position.add(new Vector3(3.1, 20, 100))
+const mountainColor = new Color(0xFF0000);
+const groundColor = new Color(0x008F00);
+scene.add(anotherLight)
+const mountainUniforms = {
+    data: {
+        value: {
+            position: anotherLight.position,
+            direction: new Vector3(0, 1, 0),
+            color: new Vector3(mountainColor.r, mountainColor.g, mountainColor.b)
+        }
+    }
+};
+
+const groundUniforms = {
+    data: {
+        value: {
+            position: anotherLight.position,
+            direction: new Vector3(0, 1, 0),
+            color: new Vector3(groundColor.r, groundColor.g, groundColor.b)
+        }
+    }
+};
+// GLSL 
+const mountainMat = new ShaderMaterial({
+    fragmentShader: _FS,
+    vertexShader: _VS,
+    uniforms: mountainUniforms
+}
+)
+
+const groundMat = new ShaderMaterial({
+    fragmentShader: _FS,
+    vertexShader: _VS,
+    uniforms: groundUniforms
+}
+)
+
+const groundGeo = new PlaneGeometry(1000, 1000);
+const ground = new Mesh(groundGeo, groundMat);
+ground.lookAt(new Vector3(0, 1, 0))
+scene.add(ground);
+
+
 // load in the road models from the scene file list
 const models = scene.models;
 console.log(models)
 const loader = new GLTFLoader()
 loadBodies(models)
 
-function loadBodies(roadModelsToLoad){
+function loadBodies(roadModelsToLoad) {
     for (let i = 0; i < roadModelsToLoad.length; i++) {
         loader.load(roadModelsToLoad[i], (gltf) => {
             let mat;
             let useMesh = true;
-            if (i == 0){
-                mat = roadMat;
+            if (i == 0) {
+                mat = mountainMat;
             } else if (i == 2) {
-                mat = new MeshToonMaterial({
-                    color: new Color(0x000049),
-                    side: DoubleSide
-                })
+                // mat = new MeshToonMaterial({
+                //     color: new Color(0x000049),
+                //     side: DoubleSide
+                // })
+                mat = mountainMat;
                 useMesh = false;
             }
             else mat = testMat;
@@ -317,7 +404,7 @@ world.addBody(killBody)
 killBody.addEventListener("collide", endGame)
 gameEndBody.addEventListener("collide", endGame)
 
-function endGame(){
+function endGame() {
     console.log("Game Over!")
     world.removeBody(boxBody)
     sceneR = gameOver;
@@ -341,7 +428,7 @@ const onAnimationFrameHandler = (timeStamp) => {
     inputControl.update();
     sound.update();
 
-    world.step(1/60);
+    world.step(1 / 60);
     //world.fixedStep();
 
     //cannonDebugger.update();
@@ -356,19 +443,19 @@ const onAnimationFrameHandler = (timeStamp) => {
 
 
     // move all physics things and move their three visualizations along with them
-    for(let i = 0; i < threeArray.length; i++){
+    for (let i = 0; i < threeArray.length; i++) {
         threeArray[i].position.copy(cannonArray[i].position);
         threeArray[i].quaternion.copy(cannonArray[i].quaternion)
     }
 
 
-    
-    overheadCamera.position.set(boxBody.position.x, boxBody.position.y+100, boxBody.position.z);
-    overheadCamera.lookAt(new Vector3(boxBody.position.x, boxBody.position.y+10, boxBody.position.z));
+
+    overheadCamera.position.set(boxBody.position.x, boxBody.position.y + 100, boxBody.position.z);
+    overheadCamera.lookAt(new Vector3(boxBody.position.x, boxBody.position.y + 10, boxBody.position.z));
 
     // GAME OVER!
     if (boxMesh.position.y <= particleSystem.geometry.attributes.position.array[1] + 2 && gamestart) {
-        renderer.render( gameover, camera );
+        renderer.render(gameover, camera);
         console.log('gameover');
         world.removeBody(boxBody);
         gamestart = false;
@@ -378,64 +465,64 @@ const onAnimationFrameHandler = (timeStamp) => {
     // renderer.setRenderTarget(null);
     // renderer.clear();
     // renderer.render(scene, camera);
-    
+
     // renderer.setRenderTarget(newBufferTexture);
     // renderer.render(scene, camera, newBufferTexture);
     // oldBufferTexture.copy(newBufferTexture);
 
     // CITATION: adapted from https://jsfiddle.net/f2Lommf5/11653/
-    renderer.setClearColor( 0x000000 );
-  
-    renderer.setViewport( 0, 0, window.innerWidth, window.innerHeight );
-    
-    if(!inMenu){
-        renderer.render(sceneR, camera );
+    renderer.setClearColor(0x000000);
+
+    renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+
+    if (!inMenu) {
+        renderer.render(sceneR, camera);
     } else {
         renderer.render(sceneR, menuCamera);
     }
-    
 
-    if(miniMap){    
-        renderer.setClearColor( 0x333333 );
-        
+
+    if (miniMap) {
+        renderer.setClearColor(0x333333);
+
         renderer.clearDepth();
-        
-        renderer.setScissorTest( true );
+
+        renderer.setScissorTest(true);
         const VIEW_X = 16;
         const VIEW_Y = 16;
         const VIEW_WIDTH = window.innerHeight / 4;
         const VIEW_HEIGHT = window.innerHeight / 4;
-        renderer.setScissor( VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT );
-        renderer.setViewport( VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT );
-    
-        renderer.render( scene, overheadCamera );
-        
-        renderer.setScissorTest( false );
+        renderer.setScissor(VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT);
+        renderer.setViewport(VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT);
+
+        renderer.render(scene, overheadCamera);
+
+        renderer.setScissorTest(false);
     }
 
-    if(miniMap){    
-        renderer.setClearColor( 0x333333 );
-        
+    if (miniMap) {
+        renderer.setClearColor(0x333333);
+
         renderer.clearDepth();
-        
-        renderer.setScissorTest( true );
-        
+
+        renderer.setScissorTest(true);
+
         const VIEW_Y = 16;
         const VIEW_WIDTH = window.innerHeight / 4;
         const VIEW_HEIGHT = window.innerHeight / 4;
         const VIEW_X = window.innerWidth - VIEW_WIDTH - 16;
-        renderer.setScissor( VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT );
-        renderer.setViewport( VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT );
-    
-        renderer.render( hud, menuCamera );
-        
-        renderer.setScissorTest( false );
+        renderer.setScissor(VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT);
+        renderer.setViewport(VIEW_X, VIEW_Y, VIEW_WIDTH, VIEW_HEIGHT);
+
+        renderer.render(hud, menuCamera);
+
+        renderer.setScissorTest(false);
     }
 
-    
+
     // end citation
-    
-   
+
+
     //scene.update && scene.update(timeStamp);
 
     //frustCull.update();
@@ -498,65 +585,66 @@ function testMouseStuff(event) {
 // ADAPTED FROM THE FOLLOWING
 // CITATION: https://solutiondesign.com/insights/webgl-and-three-js-particles/
 function createParticleSystem(particleCount) {
-   
+
     // Particles are just individual vertices in a geometry
     // Create the geometry that will hold all of the vertices
 
     const verts = [];
-   // Create the vertices and add them to the particles geometry
+    // Create the vertices and add them to the particles geometry
     for (var p = 0; p < particleCount; p++) {
         // This will create all the vertices in a range of -50 to 50 in x and -20 to 80 in z
         var x = Math.random() * 500 - 250;
         var y = 0;
         var z = Math.random() * 500 - 250;
-    
+
         // Create the vertex
         var particle = new Vector3(x, y, z);
-    
+
         // Add the vertex to the geometry
         verts.push(particle);
     }
-   
-    let particles = new BufferGeometry().setFromPoints( verts )
+
+    let particles = new BufferGeometry().setFromPoints(verts)
     // Create the material that will be used to render each vertex of the geometry
     var particleMaterial = new PointsMaterial(
-    {color: 0xffffff, 
-    size: 6,
-    map: smokeParticleTexture,
-    blending: AdditiveBlending,
-    transparent: true,
-    depthTest: true
-    });
+        {
+            color: 0xffffff,
+            size: 6,
+            map: smokeParticleTexture,
+            blending: AdditiveBlending,
+            transparent: true,
+            depthTest: true
+        });
     /* i give up this doesn't work
     /////////////////////////
     // CITATION, ADAPTED FROM http://stemkoski.github.io/Three.js/ParticleSystem-Attributes.html
     // values that are constant for all particles during a draw call
-	var customUniforms = 
-	{
-		pointTexture:   { value: smokeParticleTexture },
+    var customUniforms = 
+    {
+        pointTexture:   { value: smokeParticleTexture },
         color: { value: new Color( 0xffffff ) }
-	};
-	
-	// // properties that may vary from particle to particle. only accessible in vertex shaders!
-	// //	(can pass color info to fragment shader via vColor.)
-	// var customAttributes = 
-	// {
-	// 	customColor:   { type: "c", value: [] },
+    };
+    
+    // // properties that may vary from particle to particle. only accessible in vertex shaders!
+    // //   (can pass color info to fragment shader via vColor.)
+    // var customAttributes = 
+    // {
+    //  customColor:   { type: "c", value: [] },
     //     customSize:    { type: "c", value: []}
-	// };
+    // };
 
-	// // assign values to attributes, one for each vertex of the geometry
-	// for( var v = 0; v < particleCount; v++ ) 
-	// {
+    // // assign values to attributes, one for each vertex of the geometry
+    // for( var v = 0; v < particleCount; v++ ) 
+    // {
     //     const l = Math.random() * .5; // luminance
-	// 	customAttributes.customColor.value[ v ] = new Color(l, l, l);
+    //  customAttributes.customColor.value[ v ] = new Color(l, l, l);
     //     customAttributes.customSize.value[  v ] = 2 + Math.random() * 8; 
-	// }
+    // }
 
     var shaderMaterial = new ShaderMaterial( 
         {
-            uniforms: 		customUniforms,
-            // attributes:		customAttributes, DEPRECATED ???
+            uniforms:       customUniforms,
+            // attributes:      customAttributes, DEPRECATED ???
             vertexShader:   vertexShaderText,
             fragmentShader: fragmentShaderText,
             blending: AdditiveBlending,
@@ -570,7 +658,7 @@ function createParticleSystem(particleCount) {
 
     // Create the particle system
     return new Points(particles, particleMaterial);
-     
+
 }
 
 
@@ -586,11 +674,11 @@ function updateParticleSystem(particleSystem) {
     const numVertices = particleSystem.geometry.attributes.position.count;
 
     const pull = .000001; // tweak as necessary
-    
+
 
     // updates particle positions based on velocity
     for (let i = 0; i < numVertices; i++) {
-        vertices[ i * 3 + 0 ] += particleVelocities[i][0];
+        vertices[i * 3 + 0] += particleVelocities[i][0];
         const c = Math.abs(vertices[i * 3 + 0]);
         if (c > 300) {
             // reverses with slight nudge of randomness
@@ -599,8 +687,8 @@ function updateParticleSystem(particleSystem) {
         }
 
 
-        vertices[ i * 3 + 1 ] += .01; // y
-        vertices[ i * 3 + 2 ] += particleVelocities[i][1];
+        vertices[i * 3 + 1] += .01; // y
+        vertices[i * 3 + 2] += particleVelocities[i][1];
         const d = Math.abs(vertices[i * 3 + 2]);
         if (d > 300) {
             // reverses with slight nudge of randomness
@@ -608,21 +696,21 @@ function updateParticleSystem(particleSystem) {
             particleVelocities[i][1] += (Math.random() - .5) / 8;
         }
         if (c < 20 && d < 20) {
-            vertices[ i * 3 + 0 ] = Math.random() * 500 - 250; //resets x
-            vertices[ i * 3 + 2 ] = Math.random() * 500 - 250; //resets z
+            vertices[i * 3 + 0] = Math.random() * 500 - 250; //resets x
+            vertices[i * 3 + 2] = Math.random() * 500 - 250; //resets z
         }
 
     }
     // updates particle velocities through interactions with 
     // random subset of neighbors
-    for (let i  = 0; i < numVertices; i++) {
-        const x = vertices[ i * 3 + 0 ];
-        const z = vertices[ i * 3 + 2 ];
+    for (let i = 0; i < numVertices; i++) {
+        const x = vertices[i * 3 + 0];
+        const z = vertices[i * 3 + 2];
         for (let j = 0; j < 3; j++) {
             const v2 = Math.floor(Math.random() * particleCount);
             if (v2 == i) continue;
-            const x1 = vertices[ v2 * 3 + 0];
-            const z1 = vertices[ v2 * 3 + 2];
+            const x1 = vertices[v2 * 3 + 0];
+            const z1 = vertices[v2 * 3 + 2];
             particleVelocities[i][0] += pull * (x1 - x);
             particleVelocities[i][1] += pull * (z1 - z);
 
@@ -644,11 +732,11 @@ function updateParticleSystem(particleSystem) {
 function resetParticleSystem(particleSystem) {
 
     let vertices = particleSystem.geometry.attributes.position.array;
-    const numVertices = particleSystem.geometry.attributes.position.count;  
+    const numVertices = particleSystem.geometry.attributes.position.count;
 
     // updates particle positions based on velocity
     for (let i = 0; i < numVertices; i++) {
-        vertices[ i * 3 + 1 ] = 0; // y
+        vertices[i * 3 + 1] = 0; // y
     }
 
     particleSystem.geometry.attributes.position.needsUpdate = true;
